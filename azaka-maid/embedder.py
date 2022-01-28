d4058ef68cceb907c1eb224cb481f75c163254d1
Emiya97 <importz750@gmail.com>
2022-01-28 17:41:47 +0530

    Now uses discord.py.

---
+++ azaka-maid/embedder.py
@@ -0,0 +1,31 @@
+import textwrap
+from discord import Embed
+from azaka.objects import VN
+
+__all__ = ("vn_to_embed",)
+
+
+def vn_to_embed(vn: VN) -> Embed:
+    desc = (
+        textwrap.shorten(text=vn.description, width=300)
+        if vn.description
+        else "Not available."
+    )
+    embed = Embed(
+        title=vn.title,
+        description=desc,
+        color=0xFF0000,
+        url=f"https://www.wikidata.org/wiki/{vn.links.wikidata}"
+        if vn.links.wikidata
+        else None,
+    )
+    embed.set_thumbnail(url=vn.image)
+    embed.add_field(name="id: ", value=vn.id, inline=True)
+    embed.add_field(name="platforms: ", value=", ".join(vn.platforms), inline=True)
+    embed.add_field(name="languages: ", value=", ".join(vn.languages), inline=True)
+    embed.add_field(name="length: ", value=vn.length or "Not available.", inline=True)
+    embed.add_field(
+        name="original language: ", value=", ".join(vn.orig_lang), inline=True
+    )
+    embed.add_field(name="release date:", value=vn.released, inline=True)
+    return embed
---
+++ azaka-maid/main.py
@@ -0,0 +1,74 @@
+import asyncio
+import logging
+import os
+import typing as t
+
+import azaka
+from discord import DiscordException, Embed, Intents
+from discord.ext import commands
+from dotenv import load_dotenv
+
+from embedder import vn_to_embed
+
+logging.basicConfig(level=logging.DEBUG)
+load_dotenv()
+
+
+class Serenity(commands.Bot):
+    __slots__ = ("results", "azaka_client")
+
+    def __init__(self, **kwargs: t.Any) -> None:
+        super().__init__(command_prefix=">", intents=Intents.all(), **kwargs)
+
+        self.add_command(commands.Command(self.vn, name="vn"))
+
+        # A queue for storing results from azaka.
+        self.results: asyncio.Queue = asyncio.Queue()
+        self.azaka_client: azaka.Client = azaka.Client(loop=self.loop)
+
+    async def on_ready(self) -> None:
+        print("CONNECTED TO DISCORD.")
+
+    async def azaka_ready(self) -> None:
+        # Newer version has `wait_until_connect`.
+        await self.azaka_client._connector.on_connect.wait()
+        print("CONNECTED TO VNDB.")
+
+    async def get_vn(self, ctx: azaka.Context, title: str) -> None:
+        """Internal method which makes call to the API and puts the
+        result into a queue."""
+        try:
+            res = (await ctx.get_vn(lambda VN: VN.TITLE % title, details=True))[0]
+        except azaka.ThrottledError:
+            res = "Azaka got ratelimited."
+
+        finally:
+            await self.results.put(res)
+
+    @commands.cooldown(rate=2, per=1, type=commands.BucketType.user)
+    async def vn(self, ctx: commands.Context, *, title: str) -> t.Optional[Embed]:
+        """A command to fetch VNs from VNDB."""
+        self.azaka_client.register(self.get_vn, title=title)
+        result = await self.results.get()
+        if not isinstance(result, str):
+            return await ctx.send(embed=vn_to_embed(result))
+        await ctx.send(result)
+
+    async def on_command_error(
+        self, ctx: commands.Context, exc: DiscordException
+    ) -> None:
+        """Global Error Handler for discord.py exceptions."""
+        if isinstance(exc, commands.CommandOnCooldown):
+            await ctx.send("This command is on cooldown.")
+        else:
+            raise exc from None
+
+
+serenity = Serenity()
+
+# Not mandatory to install jishaku, just using it for personal use.
+serenity.load_extension("jishaku")
+
+serenity.loop.create_task(serenity.start(os.getenv("TOKEN")))
+serenity.loop.create_task(serenity.azaka_ready())
+serenity.azaka_client.start()
